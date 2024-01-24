17dd863b1a6bd60eb17e24f3860e0c68573e6f6e
lihuazou-asd <877644198@qq.com>
2024-01-24 23:15:26 +0800

    ABMgr+LuaMgr

---
+++ Assets/Scripts/ABMgr.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ABMgr : SingletonAutoMono<ABMgr>
+{
+    private AssetBundle mainAB = null;
+    private AssetBundleManifest manifest = null;
+    private Dictionary<string, AssetBundle> abDic = new Dictionary<string, AssetBundle>();
+
+    private string StreamingAssetsPathUrl
+    {
+        get
+        {
+            return Application.streamingAssetsPath + "/";
+        }
+    }
+    private string PersistentPathUrl
+    {
+        get
+        {
+            return Application.persistentDataPath + "/";
+        }
+    }
+    
+    public void LoadAB(string abName){
+        if(mainAB == null){
+            mainAB = AssetBundle.LoadFromFile(ExistABPath("PC"));
+            manifest = mainAB.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+        }
+        AssetBundle ab = null;
+        string[] abDependencies = manifest.GetAllDependencies(abName);
+        for(int i = 0;i<abDependencies.Length;i++){
+            if(!abDic.ContainsKey(abDependencies[i])){
+                ab = AssetBundle.LoadFromFile(ExistABPath(abDependencies[i]));
+                abDic.Add(abDependencies[i],ab);
+            }
+        }
+
+        if(!abDic.ContainsKey(abName)){
+            abDic.Add(abName,AssetBundle.LoadFromFile(ExistABPath(abName)));
+        }
+    }
+
+    public Object LoadRes(string abName,string resName,System.Type type){
+        LoadAB(abName);
+
+        Object obj = abDic[abName].LoadAsset(resName,type);
+        if(obj is GameObject){
+            return Instantiate(obj);
+        }
+        else{
+            return obj;
+        }
+
+    }
+    public Object LoadRes(string abName,string resName){
+        LoadAB(abName);
+
+        Object obj = abDic[abName].LoadAsset(resName);
+        if(obj is GameObject){
+            return Instantiate(obj);
+        }
+        else{
+            return obj;
+        }
+
+    }
+    private string ExistABPath(string abName){
+        if(File.Exists(PersistentPathUrl+abName)){
+            return PersistentPathUrl+abName;
+        }
+        else{
+            return StreamingAssetsPathUrl+name;
+        }
+    }
+
+
+    public void UnLoad(string abName)
+    {
+        if( abDic.ContainsKey(abName) )
+        {
+            abDic[abName].Unload(false);
+            abDic.Remove(abName);
+        }
+    }
+
+
+    public void ClearAB()
+    {
+        AssetBundle.UnloadAllAssetBundles(false);
+        abDic.Clear();
+        mainAB = null;
+        manifest = null;
+    }
+    
+}
---
+++ Assets/Scripts/LuaMgr.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using XLua;
+public class LuaMgr : BaseManager<LuaMgr>
+{
+    private LuaEnv luaEnv;
+
+    public LuaTable Global
+    {
+        get
+        {
+            return luaEnv.Global;
+        }
+    }
+
+    public void Init(){
+        if(luaEnv!=null) return;
+        luaEnv = new LuaEnv();
+        luaEnv.AddLoader(MyCustomLoader);
+        luaEnv.AddLoader(MyCustomABLoader);
+    }
+
+    public void DoLuaFile(string file)
+    {
+        luaEnv.DoString($"require('{file}')");
+    }
+
+    private byte[] MyCustomLoader(ref string fileName){
+        string path = Application.dataPath + "/ArtRes/Lua/"+fileName+".lua";
+        if(File.Exists(path)){
+            return File.ReadAllBytes(path);
+        }
+        else{
+            Debug.Log("MyCustomLoader重定向失败，文件名为" + path);
+        }
+        return null;
+    }
+
+    private byte[] MyCustomABLoader(ref string fileName){
+        
+        TextAsset lua = ABMgr.GetInstance().LoadRes("lua",fileName+".lua") as TextAsset;
+        
+        if(lua !=null){
+            return lua.bytes;
+        }
+        else{
+            Debug.Log("MyCustomABLoader重定向失败，文件名为：" + fileName);
+        }
+
+        return null;
+    }
+    public void DoString(string str)
+    {
+        if(luaEnv == null)
+        {
+            Debug.Log("解析器未初始化");
+            return;
+        }
+        luaEnv.DoString(str);
+    }
+
+    public void Tick()
+    {
+        if (luaEnv == null)
+        {
+            Debug.Log("解析器未初始化");
+            return;
+        }
+        luaEnv.Tick();
+    }
+    public void Dispose()
+    {
+        if (luaEnv == null)
+        {
+            Debug.Log("解析器未初始化");
+            return;
+        }
+        luaEnv.Dispose();
+        luaEnv = null;
+    }
+
+}
